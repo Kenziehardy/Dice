321efc97e7ac26aec6ac911852af9cdf5af4c18a
Kenzie <Kenzierhardy@gmail.com>
2018-07-31 17:58:41 -0700

    It works!

--- dostuff.js
+++ dostuff.js
@@ -1,6 +1,16 @@
-function dieClicked(event) {
-  event.target.innerHTML = "";
+function d6Clicked(event) {
+  event.target.innerHTML = diceRoll(6);
 }
+function d8Clicked(event) {
+  event.target.innerHTML = diceRoll(8);
+}
+function d12Clicked(event) {
+  event.target.innerHTML = diceRoll(12);
+}
+function d20Clicked(event) {
+  event.target.innerHTML = diceRoll(20);
+}
+
 function diceRoll(dieSize) {
   return Math.floor(Math.random() * dieSize + 1);
 }
@@ -10,9 +20,7 @@ var element2 = document.getElementById("die2");
 var element3 = document.getElementById("die3");
 var element4 = document.getElementById("die4");
 
-element.addEventListener("click", dieClicked);
-element2.addEventListener("click", dieClicked);
-element3.addEventListener("click", dieClicked);
-element4.addEventListener("click", dieClicked);
-
-console.log(diceRoll(6));
+element.addEventListener("click", d6Clicked);
+element2.addEventListener("click", d8Clicked);
+element3.addEventListener("click", d12Clicked);
+element4.addEventListener("click", d20Clicked);
